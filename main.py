b0fa1180ecf40f19ec24f8f0a0f1cecd9cc245ec
Apazidii <kostya.apazidi@gmail.com>
2021-04-30 01:04:08 +0300

    qq

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys  # sys нужен для передачи argv в QApplication
 from PyQt5 import QtWidgets
+import os
 import design  # Это наш конвертированный файл дизайна
 class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
     def __init__(self):
@@ -7,6 +8,19 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         # и т.д. в файле design.py
         super().__init__()
         self.setupUi(self)  # Это нужно для инициализации нашего дизайна
+        self.pushButton_2.clicked.connect(self.browse_folder)  # Выполнить функцию browse_folder
+                                                            # при нажатии кнопки
+
+    def browse_folder(self):
+        self.listWidget.clear()  # На случай, если в списке уже есть элементы
+        directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Выберите папку")
+        # открыть диалог выбора директории и установить значение переменной
+        # равной пути к выбранной директории
+
+        if directory:  # не продолжать выполнение, если пользователь не выбрал директорию
+            for file_name in os.listdir(directory):  # для каждого файла в директории
+                self.listWidget.addItem(file_name)   # добавить файл в listWidget
+
 
 def main():
     app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
