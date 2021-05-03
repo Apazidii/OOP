956fb26a842f40656c76da03732afd9ed5d2400d
apazidii <Kostya.apazidi@gmail.com>
2021-05-03 22:49:38 +0300

    тествые дизайн-файлы

---
+++ testdesign.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'testdesign.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(518, 277)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
+        self.tableWidget.setObjectName("tableWidget")
+        self.tableWidget.setColumnCount(0)
+        self.tableWidget.setRowCount(0)
+        self.horizontalLayout.addWidget(self.tableWidget)
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.label)
+        self.plainTextEdit = QtWidgets.QPlainTextEdit(self.centralwidget)
+        self.plainTextEdit.setObjectName("plainTextEdit")
+        self.verticalLayout.addWidget(self.plainTextEdit)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setObjectName("pushButton")
+        self.verticalLayout.addWidget(self.pushButton)
+        self.horizontalLayout.addLayout(self.verticalLayout)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 518, 22))
+        self.menubar.setObjectName("menubar")
+        self.File_menu = QtWidgets.QMenu(self.menubar)
+        self.File_menu.setObjectName("File_menu")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.save = QtWidgets.QAction(MainWindow)
+        self.save.setObjectName("save")
+        self.save_as = QtWidgets.QAction(MainWindow)
+        self.save_as.setObjectName("save_as")
+        self.load = QtWidgets.QAction(MainWindow)
+        self.load.setObjectName("load")
+        self.File_menu.addAction(self.save)
+        self.File_menu.addAction(self.save_as)
+        self.File_menu.addAction(self.load)
+        self.menubar.addAction(self.File_menu.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "Введите текст"))
+        self.pushButton.setText(_translate("MainWindow", "Загрузить"))
+        self.File_menu.setTitle(_translate("MainWindow", "Файл"))
+        self.save.setText(_translate("MainWindow", "Сохранить"))
+        self.save_as.setText(_translate("MainWindow", "Сохранить как"))
+        self.load.setText(_translate("MainWindow", "Загрузить"))
